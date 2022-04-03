6330c9f118771cf2044aa3927c939c8740f8c7b5
Harry Coa <rodol_2000@hotmail.com>
2022-04-03 18:48:10 -0500

    feat[react] retorno de elementros en el component

---
+++ src/PrimeraApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+// Existen 2 tipos en REACT { const , class }
+// se puede utilizar div o Fragment o vacio
+const PrimeraApp = () => {
+    return (
+        <>
+            <h1>Primera App</h1>
+            <p>Hola Mundo</p>
+        </>
+    );
+}
+
+
+
+export default PrimeraApp;

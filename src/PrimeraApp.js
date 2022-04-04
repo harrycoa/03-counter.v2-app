4f8eaddb5454f3cdc730e990233f0fe6e140e2e0
Harry Coa <rodol_2000@hotmail.com>
2022-04-03 20:55:56 -0500

    feat:[react] json stringify

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,9 +2,18 @@ import React from "react";
 // Existen 2 tipos en REACT { const , class }
 // se puede utilizar div o Fragment o vacio
 const PrimeraApp = () => {
+    // react no escribe ni booleanos ni objetos
+
+
+    const saludo = {
+        nombre: 'Juan',
+        apellido: 'Perez',
+        edad: '23',
+    }
+
     return (
         <>
-            <h1>Primera App</h1>
+            <pre> { JSON.stringify(saludo, null, 3) }</pre>
             <p>Hola Mundo</p>
         </>
     );

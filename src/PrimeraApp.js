ef8fd6eb2cf18cc2d350e81369bfa612ca78d690
Harry Coa <rodol_2000@hotmail.com>
2022-04-12 22:01:44 -0500

    feat:[props]

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,24 +1,27 @@
 import React from "react";
+import  PropTypes  from "prop-types";
 // Existen 2 tipos en REACT { const , class }
 // se puede utilizar div o Fragment o vacio
-const PrimeraApp = () => {
+const PrimeraApp = ( { saludo } ) => {
     // react no escribe ni booleanos ni objetos
 
-
-    const saludo = {
-        nombre: 'Juan',
-        apellido: 'Perez',
-        edad: '23',
+    if ( !saludo ) {
+        throw new Error("No hay saludo");
     }
+    
 
     return (
         <>
-            <pre> { JSON.stringify(saludo, null, 3) }</pre>
-            <p>Hola Mundo</p>
+            <h1> { saludo} </h1>
+            {/* <pre> { JSON.stringify(saludo, null, 3) }</pre>*/ }
+            <p>Mi primera App React</p>
         </>
     );
 }
 
 
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
 
 export default PrimeraApp;

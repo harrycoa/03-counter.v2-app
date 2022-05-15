af330862050c57973d6e6feeebe60a824324ef81
Harry Coa <rodol_2000@hotmail.com>
2022-05-15 17:58:12 -0500

    feat:[hooks] useState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import  PropTypes  from "prop-types";
 
 const CounterApp = ( { value  } ) => {
@@ -7,16 +7,21 @@ const CounterApp = ( { value  } ) => {
     if ( !value ) {
         throw new Error("No hay valor");
     }
+
+    const [ counter, setCounter ] = useState(0);
+
+
     
     // handleAdd
-    const handleAdd = (e) => {
-        console.log(e);
+    const handleAdd = () => {
+        //setCounter( counter + 1);
+        setCounter( (c)=> c + 1);
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
 
             <button onClick= {  handleAdd } >+1</button>
      

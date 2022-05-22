bdbd819f055087f59715488ff044e73162cc88bc
Harry Coa <rodol_2000@hotmail.com>
2022-05-22 17:45:26 -0500

    feat: Actualizacion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import PrimeraApp from './PrimeraApp';
 import CounterApp from './CounterApp';
 import './index.css';
 
-
-
 const divRoot = document.querySelector('#root');
-
 ReactDOM.render(<CounterApp value= { 7 } />, divRoot);
+
+
+
+
+
+// React +18
+/*
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import CounterApp from './CounterApp';
+import './index.css';
+
+const divRoot = document.querySelector('#app');
+const root = createRoot(divRoot);
+root.render(<CounterApp value= { 10 } />);
+
+*/
